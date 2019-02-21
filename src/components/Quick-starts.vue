9956b2c19a580ee5982ffaa1679f29754f611a1e
chih-wei chang <cw.chang@kyperdata.com>
2019-02-21 17:36:46 +0800

    create quick-starts component

---
+++ src/components/Quick-starts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="quick-starts">
+    <div class="quick-starts-item"
+      v-for="(item, index) in items"
+      :key="index"
+      @click="onItemClick($event, item)"
+    >
+      {{item}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'QuickStarts',
+  props: {
+    items: { type: Array, default: () => [] }
+  },
+  methods: {
+    onItemClick (e, item) {
+      this.$emit('clickItem', item)
+    }
+  }
+}
+</script>
