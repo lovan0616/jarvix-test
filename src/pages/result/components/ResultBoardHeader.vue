d6ded1dd4a3178ac5e31fe85a0f3e31fa0063c41
Christan Huang <christan@gsailors.com>
2019-03-25 14:46:40 +0800

    add ResultBoardHeader components

---
+++ src/pages/result/components/ResultBoardHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="result-board-title">
+    <div class="board-name">{{ question }}</div>
+    <div class="board-period">{{ period }}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ResultBoardHeader',
+  props: {
+    question: {
+      type: String,
+      default: ''
+    },
+    period: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.result-board-title {
+  .board-name {
+    font-size: 18px;
+    line-height: 26px;
+    letter-spacing: 0.1em;
+    color: #2E2E2E;
+    margin-bottom: 5px;
+  }
+  .board-period {
+    font-size: 14px;
+    line-height: 14px;
+    letter-spacing: 0.05em;
+    color: #979797;
+  }
+}
+</style>
