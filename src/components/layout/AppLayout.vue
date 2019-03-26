6ee1039013c4e8b8b5e011fe9033b4ddd934f15a
Christan Huang <christan@gsailors.com>
2019-03-26 14:01:05 +0800

    add app layout components

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="sy-holy-grail" >
+    <div class="wrapper">
+      <app-header></app-header>
+      <main class="main">
+        <div class="center page-result">
+          <div class="bg"></div>
+          <transition name="fade" mode="out-in">
+            <router-view ></router-view>
+          </transition>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+<script>
+import AppHeader from './AppHeader'
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppHeader
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '../../../src/styles/common/variables.scss';
+
+.main {
+  margin-top: $header-height;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import AppLayout from '@/components/layout/Applayout'
 
 Vue.use(Router)
 
@@ -12,8 +13,14 @@ export default new Router({
     },
     {
       path: '/result',
-      name: 'PageResult',
-      component: () => import('@/pages/result/Index')
+      component: AppLayout,
+      children: [
+        {
+          path: '/',
+          name: 'PageResult',
+          component: () => import('@/pages/result/Index')
+        }
+      ]
     }
   ]
 })
