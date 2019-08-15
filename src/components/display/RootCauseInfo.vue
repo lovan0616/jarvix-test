59ed16cfc11ede47e05540e1bc928200948ff9d5
christan.huang <christan.huang@kyperdata.com>
2019-08-15 20:37:43 +0800

    modify root-cause style

--- src/components/display/RootCauseInfo.vue
+++ src/components/display/RootCauseInfo.vue
@@ -12,35 +12,42 @@
         :label="rootCauseInfo.name"
         :name="rootCauseInfo.name"
       >
-        <div class="insights-info-block">
-          <div class="insight-table">
-            <div class="insight-table-row table-head">
-              <div class="insight-table-cell">筛选栏位</div>
-              <div class="insight-table-cell">筛选数值</div>
-              <div class="insight-table-cell">资料笔数</div>
-              <div class="insight-table-cell">总占比</div>
-              <div class="insight-table-cell">平均值</div>
-              <div class="insight-table-cell">备注</div>
-            </div>
-            <div class="insight-table-row"
-              v-for="(tableInfo, contentIndex) in rootCauseInfo.content"
-              :key="index +'-'+ contentIndex"
-            >
-              <div class="insight-table-cell">{{ tableInfo.columnName }}</div>
-              <div class="insight-table-cell">
-                <a class="drill-down-link"
-                  href="javascript:void(0)"
-                  v-if="tableInfo.link"
-                  @click="drillDown(tableInfo.link.question)"
-                >{{ tableInfo.columnValue }}</a>
-                <span v-else>{{ tableInfo.columnValue }}</span>
+        <div class="root-cause-container"
+          :class="{'is-open': isShowChatRoom}"
+        >
+          <div class="root-cause-card"
+            v-for="(tableInfo, contentIndex) in rootCauseInfo.content"
+            :key="index +'-'+ contentIndex"
+            @click="drillDown(tableInfo.link.question)"
+          >
+            <div class="abstract-info">
+              <div class="column-title">{{ tableInfo.columnName }}的{{ tableInfo.columnValue }}</div>
+              <div class="sub-title">{{ rootCauseInfo.name }}{{ tableInfo.diffAverageRate > 0 ? '高于' : '低于' }}平均值</div>
+              <div class="amount-block"
+                :class="{'is-special': tableInfo.unusual}"
+              >
+                <div class="count">{{ tableInfo.diffAverageRate + '%' }}</div>
+                <div class="hight-light-label"
+                  v-show="tableInfo.unusual"
+                >关键洞察</div>
               </div>
-              <div class="insight-table-cell">{{ tableInfo.count }}</div>
-              <div class="insight-table-cell">{{ tableInfo.percent }}</div>
-              <div class="insight-table-cell">{{ tableInfo.average }}</div>
-              <div class="insight-table-cell"
-                :class="{active: tableInfo.unusual}"
-              >{{ tableInfo.remark }}</div>
+            </div>
+            <div class="detail-info">
+              <div class="title">整体{{ rootCauseInfo.name }}平均值为{{ tableInfo.totalAverage }}，{{ tableInfo.columnName }}的{{ tableInfo.columnValue }}{{ tableInfo.diffAverageRate > 0 ? '高于' : '低于' }}平均值{{ tableInfo.diffAverageRate + '%' }}</div>
+              <div class="info-block">
+                <div class="single-info">
+                  <div class="info-label">资料笔数</div>
+                  <div class="info-content">{{ tableInfo.count }}</div>
+                </div>
+                <div class="single-info">
+                  <div class="info-label">总占比</div>
+                  <div class="info-content">{{ tableInfo.percent }}</div>
+                </div>
+                <div class="single-info">
+                  <div class="info-label">平均值</div>
+                  <div class="info-content">{{ tableInfo.average }}</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -95,6 +102,9 @@ export default {
         }
       })
       return info
+    },
+    isShowChatRoom () {
+      return this.$store.state.isShowChatRoom
     }
   }
 }
@@ -103,61 +113,101 @@ export default {
 .insights-info.root-cause {
   margin-bottom: 48px;
 
-  .insights-info-block {
+  .root-cause-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 
-    .insight-info {
+    &.is-open {
+      .root-cause-card {
+        width: 100%;
+
+        .abstract-info {
+          width: 25%;
+        }
+
+        .info-block {
+          justify-content: flex-start;
+        }
+        .single-info  {
+          margin-right: 32px;
+        }
+      }
+    }
+  }
+
+  .root-cause-card {
+    display: flex;
+    background-color: rgba(0, 0, 0, 0.35);
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    padding: 24px 0;
+    margin-bottom: 24px;
+    width: 48%;
+    cursor: pointer;
+
+    .abstract-info {
+      text-align: center;
+      border-right: 1px solid  #2E2E2E;
+      padding: 0 24px;
+      width: 32%;
+
+      .column-title {
+
+      }
+      .sub-title {
+        font-size: 14px;
+        line-height: 26px;
+        margin-bottom: 6px;
+      }
+      .amount-block {
+        color: #4DE2F0;
+
+        &.is-special {
+          color: #FFDF6F;
+        }
+      }
+      .count {
+        font-size: 30px;
+        line-height: 1;
+        margin-bottom: 10px;
+      }
+      .hight-light-label {
+        display: inline-block;
+        font-size: 12px;
+        line-height: 1;
+        color: #FFDF6F;
+        border: 1px solid #FFDF6F;
+        border-radius: 4px;
+        padding: 6px 6px;
+      }
+    }
+
+    .detail-info {
       display: flex;
-      font-size: 14px;
-      line-height: 20px;
-      letter-spacing: 0.05em;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 0 24px;
+      flex: 1;
 
-      .dot {
-        flex: initial;
-        width: 8px;
-        height: 8px;
-        border-radius: 50%;
-        border: 1px solid $theme-color-primary;
-        margin-top: 6px;
-        margin-right: 10px;
-      }
-
-      .insight-text {
-        flex: 1;
-      }
-
-      &:not(:last-child) {
-        margin-bottom: 8px;
+      .title {
+        margin-bottom: 14px;
       }
     }
-
-    .insight-table-cell {
-      &:first-child {
-        width: 15%;
-      }
-      &:nth-child(2) {
-        width: 15%;
-      }
-      &:nth-child(3) {
-        width: 9.87%;
-      }
-      &:nth-child(4) {
-        width: 10%;
-      }
-      &:nth-child(5) {
-        width: 9.79%;
-      }
-      &:last-child {
-        flex: 1;
-      }
-
-      &.active {
-        color: $theme-color-primary;
-      }
+    .info-block {
+      display: flex;
+      justify-content: space-between;
     }
-
-    .drill-down-link {
-      color: $theme-color-primary;
-      text-decoration: underline;
+    .single-info {
+      .info-label {
+        font-size: 14px;
+        line-height: 26px;
+        color: #A7A7A7;
+      }
+      .info-contnet {
+        font-size: 18px;
+        line-height: 26px;
+      }
     }
   }
 
