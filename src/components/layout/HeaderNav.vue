c4e3ee56f75e1fb8f8563dc48893f2b3884fe6d8
FrankYeah <aaa24295234@gmail.com>
2019-10-16 18:12:35 +0800

    wip fix layout & sort out component

--- src/components/layout/HeaderNav.vue
+++ src/components/layout/HeaderNav.vue
@@ -3,32 +3,62 @@
     <router-link class="nav-item" to="/" exact>{{ $t('nav.index') }}</router-link>
     <router-link class="nav-item" to="/pinboard">{{ $t('nav.pinboard') }}</router-link>
     <router-link class="nav-item" to="/data-management">{{ $t('nav.dataManagement') }}</router-link>
-    <set-select class="nav-select">
+    <div @mouseover="openSet" @mouseleave="closeSet" class="nav-item nav-set">
+        <div class="nav-set-flex">
+          <div>{{ $t('nav.setting') }}</div>
+          <svg-icon icon-class="dropdown" :class="{setRotate: isShow}" class="icon nav-set-icon"></svg-icon>
+        </div>
+        <set-select
+        v-if="isShow"
+        @showLang="showLangDialog"
+        @showLogout="showLogoutDialog"
+        >
+        </set-select>
+    </div>
 
-    </set-select>
-    <!-- <sy-select class="nav-select"
-      :placeholder="$t('nav.languagePlaceholder')"
-      :selected="language"
-      :items="selectItems"
-      v-on:update:selected="onSelected"
-    ></sy-select> -->
-    <!-- <div class="btn-exit"
-      @click="onBtnExitClick"
-    >
-      <svg-icon icon-class="exit" class="icon"></svg-icon>
-    </div> -->
+    <setting-dialog @closeDialog="closeLangDialog" v-if="isShowLanguage" ref="langDialog">
+        <div class="popup-lang-text">{{ $t('editing.languageSetting') }}</div>
+        <sy-select class="popup-lang-select"
+        :placeholder="$t('nav.languagePlaceholder')"
+        :selected="language"
+        :items="selectItems"
+        v-on:update:selected="onSelected"
+        ></sy-select>
+        <div class="popup-lang-flex">
+            <div @click="closeLangDialog" class="btn popup-cancel">{{ $t('button.cancel') }}</div>
+            <div @click="changeLang" class="btn btn-default popup-logout">{{ $t('button.change') }}</div>
+        </div>
+    </setting-dialog>
+
+    <setting-dialog @closeDialog="closeLogoutDialog" v-if="isShowLogout" ref="logoutDialog">
+        <div class="popup-text">{{ $t('editing.sureLogout') }}</div>
+        <div class="popup-flex">
+            <div @click="closeLogoutDialog" class="btn popup-cancel">{{ $t('button.cancel') }}</div>
+            <div @click="onBtnExitClick" class="btn btn-default popup-logout">{{ $t('button.logout') }}</div>
+        </div>
+    </setting-dialog>
   </nav>
 </template>
 <script>
 import SySelect from '@/components/select/SySelect'
 import SetSelect from '@/components/select/SetSelect'
+import SettingDialog from '@/components/dialog/SettingDialog'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'HeaderNav',
   components: {
     SySelect,
-    SetSelect
+    SetSelect,
+    SettingDialog
+  },
+  data () {
+    return {
+      isShow: false,
+      isShowLanguage: false,
+      isShowLogout: false,
+      selectedLanguage: this.language
+    }
   },
   computed: {
     ...mapGetters('profile', ['language', 'languages']),
@@ -42,13 +72,35 @@ export default {
     }
   },
   methods: {
+    openSet () {
+      this.isShow = true
+    },
+    closeSet () {
+      this.isShow = false
+    },
     onSelected (item) {
-      this.$store.dispatch('profile/updateLanguage', item)
+      this.selectedLanguage = item
     },
     onBtnExitClick () {
       this.$store.dispatch('userManagement/logout').then(() => {
         this.$router.push('/login')
       })
+    },
+    showLangDialog () {
+      this.isShowLanguage = true
+    },
+    closeLangDialog () {
+      this.isShowLanguage = false
+    },
+    changeLang () {
+      this.$store.dispatch('profile/updateLanguage', this.selectedLanguage)
+      this.isShowLanguage = false
+    },
+    showLogoutDialog () {
+      this.isShowLogout = true
+    },
+    closeLogoutDialog () {
+      this.isShowLogout = false
     }
   }
 }
@@ -91,6 +143,27 @@ export default {
     align-items: center;
     justify-content: center;
     cursor: pointer;
+  }
+
+  .nav-set {
+    position: relative;
+    cursor: pointer;
+
+    .nav-set-flex{
+      display: flex;
+      align-items: center;
+    }
+
+    .nav-set-icon{
+      margin-left: 6px;
+      width: 12px;
+      text-align: center;
+    }
+
+    .setRotate{
+      transform: rotate(180deg);
+      transition: all 0.3s;
+    }
   }
 }
 </style>
--- src/components/select/SetSelect.vue
+++ src/components/select/SetSelect.vue
@@ -1,43 +1,87 @@
 <template>
-  <div class="set-select">
-      <div>設定</div>
-      <div class="set-popup">
-          <div class="set-flex">
-              <div>icon</div>
-              <div>語言設定:簡中</div>
-          </div>
-          <div class="set-flex">
-              <div>icon</div>
-              <div>登出</div>
-          </div>
-      </div>
-  </div>
+    <div class="set-select">
+         <div class="set-select-box">
+            <div @click="showLang" class="set-flex">
+                <svg-icon icon-class="language" class="icon set-icon"></svg-icon>
+                <div>{{ $t('editing.languageSetting') }}: {{ $t('lang') }}</div>
+            </div>
+            <div @click="showLogout" class="set-flex">
+                <svg-icon icon-class="logout" class="icon set-icon"></svg-icon>
+                <div>{{ $t('button.logout') }}</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import SySelect from '@/components/select/SySelect'
 
 export default {
   name: 'SetSelect',
-  props: {
+  components: {
+    SySelect
+  },
+  methods: {
+    showLang () {
+      this.$emit('showLang')
+    },
+    showLogout () {
+      this.$emit('showLogout')
+    }
+  },
+  computed: {
+    ...mapGetters('profile', ['language', 'languages']),
+    selectItems () {
+      return Object.keys(this.languages).map(key => {
+        return {
+          id: key,
+          name: this.languages[key]
+        }
+      })
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
-.set-select{
-    position: relative;
-}
 
-.set-flex{
-    display: flex;
-}
-
-.set-popup{
-    height: 160px;
-    width: 60px;
+.set-select-box{
+    height: 80px;
+    width: 160px;
     position: absolute;
     top: 50px;
     left: -100px;
     background: #233131;
     border-radius: 8px;
+    font-size: 14px;
+
+    .set-flex{
+        height: 50%;
+        align-items: center;
+        display: flex;
+        border-bottom: 1px solid #384545;
+        color: #a7a7a7;
+    }
+
+    .set-flex:hover{
+        color: #fff;
+    }
+
+    .set-icon{
+        margin: 0px 6px;
+    }
+}
+
+.set-select-box::before{
+    content: '';
+    display: block;
+    position: absolute;
+    top: -8px;
+    right: 32px;
+    border-color: transparent transparent #233131 transparent;
+    border-style: solid;
+    border-width: 0px 10px 10px 10px;
+    height: 0px;
+    width: 0px;
 }
 </style>
