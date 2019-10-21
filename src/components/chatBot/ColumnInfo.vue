fc61ebb87bc614e1de699e6f987d652235a51469
christan.huang <christan.huang@kyperdata.com>
2019-10-21 17:28:55 +0800

    modify column info style

--- src/components/chatBot/ColumnInfo.vue
+++ src/components/chatBot/ColumnInfo.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="column-info-block">
-    <div class="single-column"
-      v-for="(columnInfo, index) in bookmarkCloumnInfoList"
-      :key="index"
-    >{{ columnInfo.name }}</div>
+    <div class="category-title">{{ $t('askHelper.columnTitle') }}</div>
+    <div class="column-table">
+      <div class="column-list">
+        <div class="single-column column-title">{{ $t('askHelper.categoryTitle') }}</div>
+        <div class="single-column"
+          v-for="(columnInfo, index) in bookmarkCloumnInfoList.category"
+          :key="'category' + index"
+        >{{ columnInfo.name }}</div>
+      </div>
+      <div class="column-list">
+        <div class="single-column column-title">{{ $t('askHelper.numericTitle') }}</div>
+        <div class="single-column"
+          v-for="(columnInfo, index) in bookmarkCloumnInfoList.numeric"
+          :key="'numeric' + index"
+        >{{ columnInfo.name }}</div>
+      </div>
+    </div>
+    <div class="category-title">{{ $t('askHelper.datavalueTitle') }}</div>
+    <div class="category-list">
+      <div class="single-column"
+        v-for="(columnInfo, index) in bookmarkCloumnInfoList.datavalue"
+        :key="'datavalue' + index"
+      >{{ columnInfo.name }}</div>
+    </div>
   </div>
 </template>
 <script>
@@ -18,6 +38,28 @@ export default {
 </script>
 <style lang="scss" scoped>
 .column-info-block {
+  .category-title {
+    margin-bottom: 8px;
+  }
+
+  .column-table {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 16px;
+
+    .column-title {
+      font-weight: bold;
+    }
+
+    .column-list {
+      width: 46%;
+    }
+  }
+  .category-list {
+    &:not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
   .single-column {
     font-size: 14px;
     line-height: 32px;
