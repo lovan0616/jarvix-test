0ea987100d43d1c7b22ffc1cd60a56aebbc71541
christan.huang <christan.huang@kyperdata.com>
2019-12-02 16:31:13 +0800

    modify token from backend to front-end

--- src/components/resultBoard/QuestionNameToken.vue
+++ src/components/resultBoard/QuestionNameToken.vue
@@ -4,7 +4,7 @@
   >
     {{ tokenInfo.word }}
     <div class="name-info-tooltip">
-      {{ tokenInfo.typeName }}
+      {{ tokenInfo.type === 'Datavalue' ? $t(`segmentationToken.${tokenInfo.type}`, {name: tokenInfo.properties.datacolumnPrimaryAlias}) : $t(`segmentationToken.${tokenInfo.type}`) }}
     </div>
   </div>
 </template>
