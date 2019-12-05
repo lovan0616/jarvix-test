ed484868d970c3710b79f0e45480706262a384ad
chih-wei chang <cw.chang@kyperdata.com>
2019-12-05 11:51:47 +0800

    add some function for handling filter list

--- src/store/modules/dataSource/index.js
+++ src/store/modules/dataSource/index.js
@@ -13,7 +13,58 @@ export default {
     appQuestion: null,
     currentQuestionInfo: null,
     historyQuestionList: [],
-    filterList: []
+    filterList: [
+      {
+        status: true,
+        restriction: [
+          {
+            type: 'range',
+            properties: {
+              dc_name: 'col_1',
+              start: '2018年1月',
+              end: '2018年11月'
+            }
+          }
+        ]
+      },
+      {
+        status: true,
+        restriction: [
+          {
+            type: 'enum',
+            properties: {
+              dc_name: 'col_2',
+              datavalues: ['US', 'CN', 'JP', 'KR']
+            }
+          }
+        ]
+      },
+      {
+        status: true,
+        restriction: [
+          {
+            type: 'compound',
+            restraints: [
+              {
+                type: 'enum',
+                properties: {
+                  dc_name: 'col_1',
+                  datavalues: ['US', 'CN', 'JP', 'KR']
+                }
+              },
+              {
+                type: 'range',
+                properties: {
+                  dc_name: 'col_1',
+                  start: '2018年1月',
+                  end: '2018年11月'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
   },
   actions,
   mutations,
