a97e4b5f1fe7cce38a899334c2edc510bddc6166
chih-wei chang <cw.chang@kyperdata.com>
2019-12-05 16:19:00 +0800

    rm tmp data

--- src/store/modules/dataSource/index.js
+++ src/store/modules/dataSource/index.js
@@ -13,58 +13,7 @@ export default {
     appQuestion: null,
     currentQuestionInfo: null,
     historyQuestionList: [],
-    filterList: [
-      {
-        status: true,
-        restriction: [
-          {
-            type: 'range',
-            properties: {
-              dc_name: 'col_1',
-              start: '2018年1月',
-              end: '2018年11月'
-            }
-          }
-        ]
-      },
-      {
-        status: true,
-        restriction: [
-          {
-            type: 'enum',
-            properties: {
-              dc_name: 'col_2',
-              datavalues: ['US', 'CN', 'JP', 'KR']
-            }
-          }
-        ]
-      },
-      {
-        status: true,
-        restriction: [
-          {
-            type: 'compound',
-            restraints: [
-              {
-                type: 'enum',
-                properties: {
-                  dc_name: 'col_1',
-                  datavalues: ['US', 'CN', 'JP', 'KR']
-                }
-              },
-              {
-                type: 'range',
-                properties: {
-                  dc_name: 'col_1',
-                  start: '2018年1月',
-                  end: '2018年11月'
-                }
-              }
-            ]
-          }
-        ]
-      }
-    ]
+    filterList: []
   },
   actions,
   mutations,
