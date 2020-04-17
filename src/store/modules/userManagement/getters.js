c2480f67384d9652b24fa13a6d5c33c037251313
Mike <mike.huang.mikank@gmail.com>
2020-04-17 17:52:37 +0800

    Add getters

--- src/store/modules/userManagement/getters.js
+++ src/store/modules/userManagement/getters.js
@@ -4,5 +4,11 @@ export default {
   },
   hasGroupPermission: state => permissionCode => {
     return state.groupPermission.includes(permissionCode)
+  },
+  getCurrentAccountId: state => {
+    return state.accountList.find(account => account.isDefault).id
+  },
+  getCurrentGroupId: state => {
+    return state.groupList.find(group => group.isDefault).groupId
   }
 }
