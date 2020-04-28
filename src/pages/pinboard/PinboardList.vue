5dfef8520897a9908f38200b4459151a4cca9df3
katness <katness.chen@sis.ai>
2020-04-28 12:00:43 +0800

    add loading-spinner to pinboard list

--- src/pages/pinboard/PinboardList.vue
+++ src/pages/pinboard/PinboardList.vue
@@ -6,21 +6,26 @@
       <svg-icon icon-class="plus" class="icon"></svg-icon>
       {{ $t('button.addNewCategory') }}
       </button>
-    <div class="pinboard-list" v-if="pinboardList.length > 0">
-      <single-pinboard
-        v-for="boardInfo in pinboardList"
-        :key="boardInfo.id"
-        :board-info="boardInfo"
-        @showEdit="showEditDialog(boardInfo)"
-        @showDelete="showDeleteDialog(boardInfo)"
-        @showShare="showShareDialog(boardInfo)"
-      >
-      </single-pinboard>
+    <spinner
+      :title="$t('editing.loading')" v-if="isLoading"
+    ></spinner>
+    <div v-else>
+      <div class="pinboard-list" v-if="pinboardList.length > 0">
+        <single-pinboard
+          v-for="boardInfo in pinboardList"
+          :key="boardInfo.id"
+          :board-info="boardInfo"
+          @showEdit="showEditDialog(boardInfo)"
+          @showDelete="showDeleteDialog(boardInfo)"
+          @showShare="showShareDialog(boardInfo)"
+        >
+        </single-pinboard>
+      </div>
+      <empty-info-block
+        v-else
+        :msg="$t('editing.emptyPinboard')"
+      ></empty-info-block>
     </div>
-    <empty-info-block
-      v-else
-      :msg="$t('editing.emptyPinboard')"
-    ></empty-info-block>
     <writing-dialog
       v-if="isShowAdd"
       :title="$t('button.addNewCategory')"
@@ -79,6 +84,7 @@ export default {
   },
   data () {
     return {
+      isLoading: true,
       isEdit: false,
       newBoardName: null,
       isShowAdd: false,
@@ -102,7 +108,9 @@ export default {
   },
   methods: {
     getPinboardInfo () {
-      this.$store.dispatch('pinboard/getPinboardList')
+      this.$store.dispatch('pinboard/getPinboardList').then(() => {
+        this.isLoading = false
+      })
     },
     addCategory () {
       this.$store.dispatch('pinboard/createPinboard', this.newBoardName).then(response => {
