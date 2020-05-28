f0f229a595cb82d0d7ea1c1a08251f1e135bf6fd
Mike <mike.huang.mikank@gmail.com>
2020-05-28 12:38:49 +0800

    Refactor dialog

--- src/components/dialog/FillDialog.vue
+++ src/components/dialog/FillDialog.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="full-create-dialog">
     <h2>{{ $t('editing.addUser') }}</h2>
-    <div @click.stop class="full-create-dialog-box">
-      <slot></slot>
+    <div 
+      class="full-create-dialog-box" 
+      @click.stop>
+      <slot/>
     </div>
-    <div @click.stop class="dialog-btn-row">
+    <div 
+      class="dialog-btn-row" 
+      @click.stop>
       <button
         :disabled="isProcessing"
-        @click="closeDialog"
-        class="btn btn-outline dialog-btn">
+        class="btn btn-outline dialog-btn"
+        @click="closeDialog">
         {{ $t('button.cancel') }}
       </button>
       <button
         :disabled="isProcessing"
-        @click="confirmBtn"
-        class="btn btn-default dialog-btn dialog-create">
-        <svg-icon v-if="isProcessing" icon-class="spinner"></svg-icon>
+        class="btn btn-default dialog-btn dialog-create"
+        @click="confirmBtn">
+        <svg-icon 
+          v-if="isProcessing" 
+          icon-class="spinner"/>
         {{ $t('button.submit') }}
       </button>
     </div>
--- src/components/dialog/SlotDialog.vue
+++ src/components/dialog/SlotDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="slot-dialog">
-    <slot name="SlotDialog"></slot>
+    <slot name="SlotDialog"/>
   </div>
 </template>
 <script>
