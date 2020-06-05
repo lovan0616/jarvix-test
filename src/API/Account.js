a77dcda39c1f900478e271cea76ec55807dfc779
Katness Chen <sbxew99@gmail.com>
2020-06-05 08:46:28 +0800

    individual/hide groupUserManagement from HeaderNav

--- src/API/Account.js
+++ src/API/Account.js
@@ -4,8 +4,8 @@ import store from '../store'
 /**
  * 取得帳戶資訊
  */
-export function getAccountInfo () {
-  const accountId = store.getters['userManagement/getCurrentAccountId']
+export function getAccountInfo (id) {
+  const accountId = id || store.getters['userManagement/getCurrentAccountId']
   return request({
     url: `/account/${accountId}`,
     method: 'GET'
