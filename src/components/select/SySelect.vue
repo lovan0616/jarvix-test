9c4b79395c5a2828d0d9d6666ec360e0f5655efb
Mike <mike.huang.mikank@gmail.com>
2020-06-11 16:42:59 +0800

    Disable select when no incoming data

--- src/components/select/SySelect.vue
+++ src/components/select/SySelect.vue
@@ -4,6 +4,7 @@
     v-model="value"
     :placeholder="placeholder"
     :no-data-text="$t('message.noData')"
+    :disabled="disableSelect"
     @change="$emit('update:selected', $event)"
   >
     <el-option
@@ -28,6 +29,11 @@ export default {
   data () {
     return {
       value: this.selected
+    }
+  },
+  computed: {
+    disableSelect () {
+      return this.items.length === 0
     }
   },
   watch: {
