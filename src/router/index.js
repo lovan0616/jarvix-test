0a2eb23fb8d9e551795cefa833a16c9b753b0c50
katness <katness.chen@sis.ai>
2020-06-11 14:45:02 +0800

    code enhace

--- src/router/index.js
+++ src/router/index.js
@@ -218,11 +218,7 @@ const router = new Router({
               component: () => import('@/pages/groupUserManagement/Index'),
               beforeEnter: (to, from, next) => {
                 // 個人版 不能進入成員管理頁面
-                if (store.state.userManagement.license.maxUser !== 1) {
-                  next()
-                } else {
-                  router.push(from)
-                }
+                store.state.userManagement.license.maxUser > 1 ? next() : next(from)
               },
               children: [
                 {
@@ -294,7 +290,7 @@ router.beforeEach(async (to, from, next) => {
   // 處理頁面重整時 store 為空需重新取得使用者資料
   const userName = store.state.userManagement.userName
   if (!userName) await store.dispatch('userManagement/getUserInfo')
-
+  console.log('route 123')
   // 確認 account 和 group 權限都符合
   const hasPermission = store.getters['userManagement/hasPermission']
   for (let i = 0; i < to.matched.length; i++) {
