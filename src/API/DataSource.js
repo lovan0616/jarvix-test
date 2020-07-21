3d83b6ed925b2abfc36686b530aed0d8842664a3
Tsao Ying <exist02593@gmail.com>
2020-07-21 09:58:07 +0800

    Fix: datacolumn api

--- src/API/DataSource.js
+++ src/API/DataSource.js
@@ -65,7 +65,7 @@ export function renameDataSourceById (dataSourceId, name) {
 /**
  * get dataSource column info
  * @param {Number} dataSourceId - 資料源 ID
- * @param {Array} columns - 篩選的欄位(帶入勾選的選項, 全部選為 null)
+ * @param {Array} columns - 篩選的欄位(帶入勾選的選項, 全部選為 null, 都沒選為 [])
  */
 export function getDataSourceColumnInfoById(dataSourceId, dataFrameId, columns = null) {
   return request({
@@ -83,7 +83,7 @@ export function getDataSourceColumnInfoById(dataSourceId, dataFrameId, columns =
 /**
  * get dataSource data value
  * @param {Number} dataSourceId - 資料源 ID
- * @param {Array} columns - 篩選的欄位(帶入勾選的選項, 全部選為 null)
+ * @param {Array} columns - 篩選的欄位(帶入勾選的選項, 全部選為 null, 都沒選為 [])
  */
 export function getDataSourceDataValueById(dataSourceId, dataFrameId, columns = null, size = 50) {
   return request({
