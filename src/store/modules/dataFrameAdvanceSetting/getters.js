4099c6efc21d4eb8006c598d31bc724f9a39f17a
christan.huang <christan.huang@kyperdata.com>
2020-08-06 14:49:40 +0800

    modify selectedColumnList

--- src/store/modules/dataFrameAdvanceSetting/getters.js
+++ src/store/modules/dataFrameAdvanceSetting/getters.js
@@ -1,6 +1,8 @@
 export default {
   selectedColumnList(state) {
     if (state.columnList === null) return null
+    // 如果全部欄位都有選
+    if (!state.columnList.some(element => !element.isSelected)) return null
     return state.columnList.reduce((acc, cur) => {
       if (cur.isSelected) acc.push(cur.id)
       return acc
