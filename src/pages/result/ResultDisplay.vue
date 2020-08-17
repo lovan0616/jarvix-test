ced090e4926e6b53ff8440f9953fc375efb60f96
Mike <mike.huang.mikank@gmail.com>
2020-08-17 16:25:00 +0800

    Disable getting related question list feature

--- src/pages/result/ResultDisplay.vue
+++ src/pages/result/ResultDisplay.vue
@@ -165,7 +165,7 @@ export default {
           }).then(res => {
             this.$store.commit('dataSource/setCurrentQuestionInfo', null)
             this.getComponentV2(res)
-            this.getRelatedQuestion(res.resultId)
+            // this.getRelatedQuestion(res.resultId)
           }).catch(() => {
             this.isLoading = false
             this.$store.commit('chatBot/updateAnalyzeStatus', false)
@@ -181,7 +181,7 @@ export default {
         }).then(res => {
           this.$store.commit('dataSource/setCurrentQuestionInfo', null)
           this.getComponent(res)
-          this.getRelatedQuestion(res.resultId)
+          // this.getRelatedQuestion(res.resultId)
         }).catch(() => {
           this.isLoading = false
           this.$store.commit('chatBot/updateAnalyzeStatus', false)
@@ -229,7 +229,7 @@ export default {
                 selectedColumnList: this.selectedColumnList
               }).then(res => {
                 this.getComponentV2(res)
-                this.getRelatedQuestion(res.resultId)
+                // this.getRelatedQuestion(res.resultId)
               }).catch((error) => {
                 if (error.constructor.name !== 'Cancel') this.isLoading = false
               })
@@ -274,7 +274,7 @@ export default {
               selectedColumnList: this.selectedColumnList
             }).then(res => {
               this.getComponent(res)
-              this.getRelatedQuestion(res.resultId)
+              // this.getRelatedQuestion(res.resultId)
             }).catch((error) => {
               if (error.constructor.name !== 'Cancel') this.isLoading = false
             })
