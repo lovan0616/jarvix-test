a76b4b5d8a7076da555cd21614756746da1c72be
christan.huang <christan.huang@kyperdata.com>
2020-09-02 19:36:45 +0800

    move loading status position

--- src/pages/result/ResultDisplay.vue
+++ src/pages/result/ResultDisplay.vue
@@ -145,8 +145,8 @@ export default {
       this.closeUnknowInfoBlock()
     },
     fetchApiAsk (data) {
-      this.clearLayout()
       this.isLoading = true
+      this.clearLayout()
       this.$store.commit('chatBot/addUserConversation', data.question)
       this.$store.commit('chatBot/updateAnalyzeStatus', true)
       // 動態變更 title 為了方便前一頁、下一頁變更時可以快速找到
