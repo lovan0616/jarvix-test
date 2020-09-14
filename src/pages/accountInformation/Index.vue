da0b132462914258b6c2f7677fec97e77c77c993
Tsao Ying <exist02593@gmail.com>
2020-09-14 14:33:12 +0800

    Implement account usage status template

--- src/pages/accountInformation/Index.vue
+++ src/pages/accountInformation/Index.vue
@@ -3,65 +3,104 @@
     <div class="page-title-row">
       <h1 class="title">{{ $t('sideNav.accountInformation') }}</h1>
     </div>
-    <section class="info-wrapper">
+    <section class="account-info-block">
+      <h2 class="account-info-block__title">
+        {{ $t('accountInfo.accountLimitation') }}
+      </h2>
       <spinner
         v-if="isLoading"
         class="spinner"
       />
       <template v-else>
-        <div class="info-block">
-          <h3 class="info-title">
-            {{ $t('accountInfo.userAmount') }}
-          </h3>
-          <div class="sub-info-block">
-            <h6 class="sub-info-title">
-              {{ $t('accountInfo.currentUserAmount') }}
-            </h6>
-            <p class="sub-info-desciption">
-              {{ license.currentUser || '-' }}
-              {{ $tc('accountInfo.person', license.currentUser) }}
-            </p>
+        <div class="info-wrapper">
+          <div class="info-block">
+            <h3 class="info-title">
+              {{ $t('accountInfo.userAmount') }}
+            </h3>
+            <div class="sub-info-block">
+              <h6 class="sub-info-title">
+                {{ $t('accountInfo.currentUserAmount') }}
+              </h6>
+              <p class="sub-info-desciption">
+                {{ license.currentUser || '-' }}
+                {{ $tc('accountInfo.person', license.currentUser) }}
+              </p>
+            </div>
+            <div class="sub-info-block">
+              <h6 class="sub-info-title">
+                {{ $t('accountInfo.userAmountLimit') }}
+              </h6>
+              <p class="sub-info-desciption">
+                {{ license.maxUser === -1 ? '&#8734;' : (`${license.maxUser} ${$tc('accountInfo.person', license.maxUser)}` || '-') }}
+              </p>
+            </div>
           </div>
-          <div class="sub-info-block">
-            <h6 class="sub-info-title">
-              {{ $t('accountInfo.userAmountLimit') }}
-            </h6>
-            <p class="sub-info-desciption">
-              {{ license.maxUser === -1 ? '&#8734;' : (`${license.maxUser} ${$tc('accountInfo.person', license.maxUser)}` || '-') }}
-            </p>
+          <div class="info-block">
+            <h3 class="info-title">
+              {{ $t('accountInfo.dataAllowance') }}
+            </h3>
+            <div class="sub-info-block">
+              <h6 class="sub-info-title">
+                {{ $t('accountInfo.currentDataUsage') }}
+              </h6>
+              <p class="sub-info-desciption">
+                {{ shortenDataCapacityNumber(license.currentDataStorageSize) || '-' }}
+              </p>
+            </div>
+            <div class="sub-info-block">
+              <h6 class="sub-info-title">
+                {{ $t('accountInfo.dataUsageProvided') }}
+              </h6>
+              <p class="sub-info-desciption">
+                {{ license.showMaxDataStorageSize === -1 ? '&#8734;' : (shortenDataCapacityNumber(license.showMaxDataStorageSize) || '-') }}
+              </p>
+            </div>
+          </div>
+          <div class="info-block">
+            <h3 class="info-title">
+              {{ $t('accountInfo.expireDate') }}
+            </h3>
+            <div class="sub-info-block">
+              <p class="sub-info-desciption">
+                {{ license.expiredTime | convertTimeStamp }}
+              </p>
+            </div>
           </div>
         </div>
-        <div class="info-block">
-          <h3 class="info-title">
-            {{ $t('accountInfo.dataAllowance') }}
+      </template>
+    </section>
+    
+    <section class="account-info-block usage-status">
+      <h2 class="account-info-block__title">
+        {{ $t('accountInfo.accountUsageStatus') }}
+      </h2>
+      <h3 class="account-info-block__description">
+        {{ $t('accountInfo.projectAmount', {projectAmount}) }}
+      </h3>
+      <spinner
+        v-if="isLoading"
+        class="spinner"
+      />
+      <template v-else>
+        <div class="usage-status__item item">
+          <h3 class="item__title">
+            市場行銷分析
           </h3>
-          <div class="sub-info-block">
-            <h6 class="sub-info-title">
-              {{ $t('accountInfo.currentDataUsage') }}
-            </h6>
-            <p class="sub-info-desciption">
-              {{ shortenDataCapacityNumber(license.currentDataStorageSize) || '-' }}
-            </p>
+          <div class="item__info">
+            <h3 class="item__info--title"> {{ $t('accountInfo.dataframeAmount') }} </h3>
+            <div class="item__info--amount"> 12 </div>
           </div>
-          <div class="sub-info-block">
-            <h6 class="sub-info-title">
-              {{ $t('accountInfo.dataUsageProvided') }}
-            </h6>
-            <p class="sub-info-desciption">
-              {{ license.showMaxDataStorageSize === -1 ? '&#8734;' : (shortenDataCapacityNumber(license.showMaxDataStorageSize) || '-') }}
-            </p>
+          <div class="item__info">
+            <h3 class="item__info--title"> {{ $t('accountInfo.pinboardAmount') }} </h3>
+            <div class="item__info--amount"> 9999 </div>
           </div>
+          <div class="item__info">
+            <h3 class="item__info--title"> {{ $t('accountInfo.warroomAmount') }} </h3>
+            <div class="item__info--amount"> 9999 </div>
+          </div>
+
         </div>
-        <div class="info-block">
-          <h3 class="info-title">
-            {{ $t('accountInfo.expireDate') }}
-          </h3>
-          <div class="sub-info-block">
-            <p class="sub-info-desciption">
-              {{ license.expiredTime | convertTimeStamp }}
-            </p>
-          </div>
-        </div>
+
       </template>
     </section>
   </div>
@@ -113,15 +152,43 @@ export default {
   }
 }
 
-.info-wrapper {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: repeat(2, 76px) 50px;
-  grid-gap: 12px;
+.account-info-block {
+  margin-bottom: 16px;
   background: var(--color-bg-5);
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.12);
   border-radius: 8px;
   padding: 24px 20px;
+
+  &__title {
+    position: relative;
+    margin: 0 0 8px 15px;
+    font-size: 20px;
+    line-height: 32px;
+
+    &::before {
+      position: absolute;
+      top: 13px;
+      left: -13px;
+      content: "";
+      width: 6px;
+      height: 6px;
+      background: #4DE2F0;
+    }
+  }
+
+  &__description {
+    margin: 0 0 8px 0;
+    color: #CCCCCC;
+    font-size: 16px;
+    line-height: 26px;
+  }
+}
+
+.info-wrapper { 
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(2, 76px) 50px;
+  grid-gap: 12px;
 
   .spinner {
     grid-row: 2 / 3;
@@ -166,6 +233,53 @@ export default {
     margin: 0;
     font-size: 18px;
     color: #FFFFFF;
+  }
+}
+
+.usage-status {
+
+  .item {
+    display: flex;
+    align-items: center;
+    height: 80px;
+    padding: 16px 64px 20px 20px;
+    background: rgba(35, 61, 64, 0.6);
+
+    &__title {
+      flex: 1;
+      margin: 0;
+      font-size: 16px;
+      line-height: 26px;
+    }
+
+    &__info {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      &:not(:last-child) {
+        margin-right: 40px;
+      }
+
+      &--title {
+        margin-right: 8px;
+        color: #CCCCCC;
+        font-size: 16px;
+        line-height: 24px;
+      }
+
+      &--amount {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        border: 1px solid var(--color-theme);
+        font-size: 14px;
+        line-height: 20px;
+      }
+    }
   }
 }
 </style>
