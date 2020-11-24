3972fafc17a87d8ceb2de8be70f74d0ecb00badb
Tsao Ying <exist02593@gmail.com>
2020-11-24 11:01:01 +0800

    handle warning messge

--- src/components/display/DisplayLineConfidentialIntervalChart.vue
+++ src/components/display/DisplayLineConfidentialIntervalChart.vue
@@ -42,13 +42,15 @@
         class="description__item">{{ dataset.descriptions.length > 1 ? (index + 1) + '. ' + description : description }}</span>
     </div>
     <!-- <div 
-      v-if="dataset.errorMessage && dataset.errorMessage.length > 0"
+      v-if="dataset.warning && dataset.warning.length > 0"
       class="description"
     >
       <span 
-        v-for="(message, index) in dataset.errorMessage" 
+        v-for="(message, index) in dataset.warning" 
         :key="index" 
-        class="description__item description__item--warning">{{ dataset.errorMessage.length > 1 ? (index + 1) + '. ' + message : message }}</span>
+        class="description__item description__item--warning">
+        {{ dataset.warning.length > 1 ? (index + 1) + '. ' + message : message }}
+      </span>
     </div> -->
   </div>
 </template>
