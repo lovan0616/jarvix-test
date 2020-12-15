09e53e679cea6aada5e6700650ceadc23f0864d7
Tsao Ying <exist02593@gmail.com>
2020-12-15 14:10:42 +0800

    change url

--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
 window.env = window.env || {}
-window.env.PUBLIC_API_ROOT_URL = 'https://public.sp2.sis.ai/'
+window.env.PUBLIC_API_ROOT_URL = 'https://public.dev.sis.ai/'
 window.env.SCHEDULE_API_ROOT_URL = 'https://schedule.dev.sis.ai'
 window.env.SAME_ORIGIN = false
