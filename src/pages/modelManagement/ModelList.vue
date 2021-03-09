40c14bdf9648671911f0d5ce47ce637f944dfd2d
Tsao Ying <exist02593@gmail.com>
2021-03-09 15:33:45 +0800

    Add delete model dialog

--- src/pages/modelManagement/ModelList.vue
+++ src/pages/modelManagement/ModelList.vue
@@ -23,33 +23,35 @@
         :headers="tableHeaders"
         :data-list.sync="modelData"
         :loading="isLoading"
-      >
-        <!-- <template v-slot:action="{ data }">
-          <a
-            :disabled="false"
-            class="link action-link"
-            @click="">{{ $t('model.managementSetting') }}</a>
-          <a
-            :disabled="false"
-            class="link action-link"
-            @click="isShowDeleteDialog = true">{{ $t('button.delete') }}</a>
-        </template> -->
-      </crud-table>
+        @delete="confirmDelete"
+      />
     </div>
+    <decide-dialog
+      v-if="isShowDeleteDialog"
+      :content="$t('model.deleteConfirmText')"
+      :type="'delete'"
+      class="model-delete-dialog"
+      @closeDialog="cancelDelete"
+      @confirmBtn="deleteModel"
+    />
   </div>
 </template>
 <script>
 import CrudTable from '@/components/table/CrudTable'
+import DecideDialog from '@/components/dialog/DecideDialog'
+import { Message } from 'element-ui'
 
 export default {
   name: 'ModelList',
   components: {
-    CrudTable
+    CrudTable,
+    DecideDialog
   },
   data () {
     return {
       isLoading: false,
       isShowDeleteDialog: false,
+      deleteModelId: null,
       modelData: [{
         createTime: "2021-02-26T08:03:34.961+0000",
         creator: "exist02593",
@@ -115,6 +117,57 @@ export default {
         }
       ]
     }
+  },
+  mounted () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      // this.isLoading = true
+      // return this.$store.dispatch('dataSource/getDataSourceList', {})
+      //   .then(() => {
+      //     this.isLoading = false
+      //   }).catch(() => {
+      //     this.isLoading = false
+      //   })
+    },
+    confirmDelete ({id}) {
+      this.deleteModelId = id
+      this.isShowDeleteDialog = true
+    },
+    cancelDelete () {
+      this.deleteModelId = null
+      this.isShowDeleteDialog = false
+    },
+    deleteModel () {
+      console.log(this.deleteModelId)
+      // deleteDataSourceById(this.deleteId)
+      //   .then(response => {
+      //     // 清除此資料源在建置中的資料表
+      //     this.$store.commit('dataSource/updateProcessingDataFrameListAfterDeleteDataSource', this.deleteId)
+      //     this.fetchData()
+      //       .then(() => {
+      //         Message({
+      //           message: this.$t('message.deleteSuccess'),
+      //           type: 'success',
+      //           duration: 3 * 1000,
+      //           showClose: true
+      //         })
+      //         this.cancelDelete()
+      //         resolve()
+      //       })
+      //   }).catch(() => {
+      //     resolve()
+      //   })
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.model-delete-dialog {
+  >>> .dialog-content-wrapper > .content {
+    color: #FFF;
+  }
+}
+
+</style>
