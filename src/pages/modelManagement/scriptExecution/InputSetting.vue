54e439d0a356ae2193c9cb8095cb0fd260bccced
Mike <mike.huang.mikank@gmail.com>
2021-05-06 11:18:03 +0800

    Hide certain stats type options

--- src/pages/modelManagement/scriptExecution/InputSetting.vue
+++ src/pages/modelManagement/scriptExecution/InputSetting.vue
@@ -22,7 +22,7 @@
           <model-column-setting-card
             v-for="column in columnList"
             :column-info="column"
-            :data-type-option-list="statsTypeOptionList"
+            :data-type-option-list="availableStatsTypeOptionList"
             :column-list="columnList"
             :key="column.id"
             @updateDataColumn="updateDataColumn($event, column.id)"
@@ -85,6 +85,10 @@ export default {
     ...mapState('modelManagement', ['currentUploadModelInfo']),
     modelColumnNames () {
       return this.columnList.map(item => item.modelColumnName)
+    },
+    availableStatsTypeOptionList () {
+      const availableStatsTypeSet = new Set(['CATEGORY', 'NUMERIC'])
+      return statsTypeOptionList.filter(statsType => availableStatsTypeSet.has(statsType.value))
     }
   },
   mounted () {
