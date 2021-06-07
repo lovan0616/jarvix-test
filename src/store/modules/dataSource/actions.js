de443072c92df2fdee81a01bcd534550ebf5dcc1
christan.huang <christan.huang@kyperdata.com>
2021-06-07 23:04:34 +0800

    refactor DataFrameMenu && fix DataFrameMenu sync after delete dataFrame

--- src/store/modules/dataSource/actions.js
+++ src/store/modules/dataSource/actions.js
@@ -268,10 +268,7 @@ export default {
     if (state.dataFrameId === "all" || hasCurrentDataFrame) return
 
     // 如果 dataFrame 被刪掉則恢復預設 all
-    getDataSourceList().then(res => {
-      commit('setDataSourceList', res)
-      return dispatch('changeDataFrameById', 'all')
-    })
+    return dispatch('changeDataFrameById', 'all')
   },
   triggerColumnDataCalculation({ state, getters }) {
     const restrictions = getters.filterRestrictionList
