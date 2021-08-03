135bdb19a3d1aaef2e6ea120c3cab5b1b90c00e9
LiamWu <lovan0616@gmail.com>
2021-08-03 15:42:44 +0800

    add vi-VN.json and VN options

--- src/lang/index.js
+++ src/lang/index.js
@@ -3,19 +3,22 @@ import VueI18n from 'vue-i18n'
 import zhCN from './zh-CN.json'
 import zhTW from './zh-TW.json'
 import enUS from './en-US.json'
+import viVN from './vi-VN.json'
 import scheduleTW from '@/schedule/lang/zh-TW.json'
 import scheduleCN from '@/schedule/lang/zh-CN.json'
 import scheduleEN from '@/schedule/lang/en-US.json'
 import enLocale from 'element-ui/lib/locale/lang/en'
 import zhTWLocale from 'element-ui/lib/locale/lang/zh-TW'
 import zhCNLocale from 'element-ui/lib/locale/lang/zh-CN'
+import viVNLocale from 'element-ui/lib/locale/lang/vi'
 
 Vue.use(VueI18n)
 const locale = 'zh-TW'
 const messages = {
   'zh-CN': { ...zhCN, ...scheduleCN, ...zhCNLocale },
   'zh-TW': { ...zhTW, ...scheduleTW, ...zhTWLocale },
-  'en-US': { ...enUS, ...scheduleEN, ...enLocale }
+  'en-US': { ...enUS, ...scheduleEN, ...enLocale },
+  'vi-VN': { ...viVN, ...viVNLocale }
 }
 
 const i18n = new VueI18n({
