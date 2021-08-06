7683027e40f50bb9e188d0efb86f0b16d006cca3
LiamWu <lovan0616@gmail.com>
2021-08-06 09:59:52 +0800

    temp: use scheduleCN

--- src/lang/index.js
+++ src/lang/index.js
@@ -12,13 +12,14 @@ import zhTWLocale from 'element-ui/lib/locale/lang/zh-TW'
 import zhCNLocale from 'element-ui/lib/locale/lang/zh-CN'
 import viVNLocale from 'element-ui/lib/locale/lang/vi'
 
+// Todo: 排程越南文暫時以中文檔替代，待越南文完成需置換
 Vue.use(VueI18n)
 const locale = 'zh-TW'
 const messages = {
   'zh-CN': { ...zhCN, ...scheduleCN, ...zhCNLocale },
   'zh-TW': { ...zhTW, ...scheduleTW, ...zhTWLocale },
   'en-US': { ...enUS, ...scheduleEN, ...enLocale },
-  'vi-VN': { ...viVN, ...viVNLocale }
+  'vi-VN': { ...viVN, ...scheduleCN, ...viVNLocale }
 }
 
 const i18n = new VueI18n({
