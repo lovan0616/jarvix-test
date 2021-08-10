20487164503d24a8ed32596ea9fd1cdd93af1706
William Chen <william.chen@sis.ai>
2021-08-10 15:00:02 +0800

    Move localStorage operation in to IIFE

--- public/static/config.js
+++ public/static/config.js
@@ -1,7 +1,11 @@
 window.env = window.env || {}
-window.env.PUBLIC_API_ROOT_URL = localStorage.getItem('PUBLIC_API_ROOT_URL') || 'https://public.dev.sis.ai/'
-window.env.SCHEDULE_API_ROOT_URL = localStorage.getItem('SCHEDULE_API_ROOT_URL') || 'https://schedule.dev.sis.ai/'
+window.env.PUBLIC_API_ROOT_URL = 'https://public.dev.sis.ai/'
+window.env.SCHEDULE_API_ROOT_URL = 'https://schedule.dev.sis.ai/'
 window.env.SAME_ORIGIN = false
 
-localStorage.setItem('PUBLIC_API_ROOT_URL', window.env.PUBLIC_API_ROOT_URL)
-localStorage.setItem('SCHEDULE_API_ROOT_URL', window.env.SCHEDULE_API_ROOT_URL)
+;(function initForDev () {
+  window.env.PUBLIC_API_ROOT_URL = localStorage.getItem('PUBLIC_API_ROOT_URL') || window.env.PUBLIC_API_ROOT_URL
+  window.env.SCHEDULE_API_ROOT_URL = localStorage.getItem('SCHEDULE_API_ROOT_URL') || window.env.SCHEDULE_API_ROOT_URL
+  localStorage.setItem('PUBLIC_API_ROOT_URL', window.env.PUBLIC_API_ROOT_URL)
+  localStorage.setItem('SCHEDULE_API_ROOT_URL', window.env.SCHEDULE_API_ROOT_URL)
+})()
