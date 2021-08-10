bcaa0be043a0782214ca4f3b46707a4da1da5982
William Chen <william.chen@sis.ai>
2021-08-10 14:40:59 +0800

    Move api root options into sisSetting

--- public/static/config.js
+++ public/static/config.js
@@ -3,23 +3,5 @@ window.env.PUBLIC_API_ROOT_URL = localStorage.getItem('PUBLIC_API_ROOT_URL') || 
 window.env.SCHEDULE_API_ROOT_URL = localStorage.getItem('SCHEDULE_API_ROOT_URL') || 'https://schedule.dev.sis.ai/'
 window.env.SAME_ORIGIN = false
 
-window.env.PUBLIC_API_ROOT_OPTIONS = {
-  dev: 'https://public.dev.sis.ai/',
-  qa: 'https://public.qa.sis.ai/',
-  qa2: 'https://public.qa2.sis.ai/',
-  sp1: 'https://public.sp1.sis.ai/',
-  sp2: 'https://public.sp2.sis.ai/',
-  sp3: 'https://public.sp3.sis.ai/'
-}
-
-window.env.SCHEDULE_API_ROOT_OPTIONS = {
-  dev: 'https://schedule.dev.sis.ai/',
-  qa: 'https://schedule.qa.sis.ai/',
-  qa2: 'https://schedule.qa2.sis.ai/',
-  sp1: 'https://schedule.sp1.sis.ai/',
-  sp2: 'https://schedule.sp2.sis.ai/',
-  sp3: 'https://schedule.sp3.sis.ai/'
-}
-
 localStorage.setItem('PUBLIC_API_ROOT_URL', window.env.PUBLIC_API_ROOT_URL)
 localStorage.setItem('SCHEDULE_API_ROOT_URL', window.env.SCHEDULE_API_ROOT_URL)
--- src/pages/sisSetting/Index.vue
+++ src/pages/sisSetting/Index.vue
@@ -156,6 +156,24 @@
   </div>
 </template>
 <script>
+const PUBLIC_API_ROOT_OPTIONS = {
+  dev: 'https://public.dev.sis.ai/',
+  qa: 'https://public.qa.sis.ai/',
+  qa2: 'https://public.qa2.sis.ai/',
+  sp1: 'https://public.sp1.sis.ai/',
+  sp2: 'https://public.sp2.sis.ai/',
+  sp3: 'https://public.sp3.sis.ai/'
+}
+
+const SCHEDULE_API_ROOT_OPTIONS = {
+  dev: 'https://schedule.dev.sis.ai/',
+  qa: 'https://schedule.qa.sis.ai/',
+  qa2: 'https://schedule.qa2.sis.ai/',
+  sp1: 'https://schedule.sp1.sis.ai/',
+  sp2: 'https://schedule.sp2.sis.ai/',
+  sp3: 'https://schedule.sp3.sis.ai/'
+}
+
 export default {
   name: 'PageSisSetting',
   data () {
@@ -170,9 +188,9 @@ export default {
       joinLimit: localStorage.getItem('joinLimit'),
       newParser: localStorage.getItem('newParser'),
       publicApiRootUrl: localStorage.getItem('PUBLIC_API_ROOT_URL'),
-      publicApiRootOptions: Object.entries(window.env?.PUBLIC_API_ROOT_OPTIONS ?? {}).map(([label, value]) => ({ label, value })),
+      publicApiRootOptions: Object.entries(PUBLIC_API_ROOT_OPTIONS).map(([label, value]) => ({ label, value })),
       scheduleApiRootUrl: localStorage.getItem('SCHEDULE_API_ROOT_URL'),
-      scheduleApiRootOptions: Object.entries(window.env?.SCHEDULE_API_ROOT_OPTIONS ?? {}).map(([label, value]) => ({ label, value }))
+      scheduleApiRootOptions: Object.entries(SCHEDULE_API_ROOT_OPTIONS).map(([label, value]) => ({ label, value }))
     }
   },
   computed: {
